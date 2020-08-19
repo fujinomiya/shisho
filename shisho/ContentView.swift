19a8f36d4e01ae481050532be8c5a196cc09a45a
fujinomiya <fujinomiya@protonmail.com>
2020-08-19 10:17:26 +0300

    Initial Commit

---
+++ shisho/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shisho
+//
+//  Created by fujinomiya on 19.8.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ shisho/shishoApp.swift
@@ -0,0 +1,17 @@
+//
+//  shishoApp.swift
+//  shisho
+//
+//  Created by fujinomiya on 19.8.2020.
+//
+
+import SwiftUI
+
+@main
+struct shishoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
